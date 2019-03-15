a281df31691f107cbbec5e24065fb2e3c3f09282
Ho <jecdis@outlook.com>
2019-03-15 12:50:04 -0700

    add all types

--- templates/src/main/java/templates/DataTypes.java
+++ templates/src/main/java/templates/DataTypes.java
@@ -1,8 +1,28 @@
 package templates;
 /**
- *
+ * 
  */
 public class DataTypes {
 	public static void main(String[] args) {
+	    boolean o = true;
+	    char    c = 'a';
+	    
+	    byte    b = 1;
+	    short   s = 10;
+	    int     i = 100;
+	    long    l = 1000;
+	    
+	    float  f  = 1.0f;
+	    double d  = 1.0;
+	    
+	    //print all
+	    System.out.println("o = " + o);
+	    System.out.println("c = " + c);
+	    System.out.println("b = " + b);
+	    System.out.println("s = " + s);
+	    System.out.println("i = " + i);
+	    System.out.println("l = " + l);
+	    System.out.println("f = " + f);
+	    System.out.println("d = " + d);
 	}
 }
